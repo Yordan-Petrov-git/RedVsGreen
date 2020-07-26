d01d6746cef1d0ef747323e6d7e68f0d4fa0f39f
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-07-26 15:11:29 +0300

    Bug fixed with updating the grid elem after applying the rules to the generation.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,43 +8,49 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) {
-
-
-        int[][] array = {
-                {0, 0, 0},
-                {1, 1, 1},
-                {0, 0, 0}
-        };
-
-
-
+//
 //        int[][] array = {
-//                {1, 0, 0, 1},
-//                {1, 1, 1, 1},
-//                {0, 1, 0, 0},
-//                {1, 0, 1, 0}
+//                {0, 0, 0},
+//                {1, 1, 1},
+//                {0, 0, 0}
 //        };
 
-        List<GridElement> gridElementList = makeGridElements(array);
+        int[][] array = {
+                {1, 0, 0, 1},
+                {1, 1, 1, 1},
+                {0, 1, 0, 0},
+                {1, 0, 1, 0}
+        };
 
+        List<GridElement> gridElementList = makeGridElements(array);
         //TODO FIX WRONG NEIGHBOUR COUNT ON SECOND ITERATION
-        int repeatCounter = 2;
+        //TODO POSSIBLY UPDATE NEIGHBOURS IN ELEMENTS
+        int repeatCounter = 10;
 
         while (repeatCounter > 0) {
-
             for (int i = 0; i < array.length; i++) {
                 for (int j = 0; j < array[i].length; j++) {
-
 
                     GridElement elem = getElementAtRowColumnFromElementList(i, j, gridElementList);
                     assert elem != null;
                     rules(array, elem);
-
                 }
             }
+
+            //TODO ITERATES TROUGH THE ARRAY
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    //TODO GETS EACH I J ELEMENT
+                    GridElement elem = getElementAtRowColumnFromElementList(i, j, gridElementList);
+                    //TODO AND RECALCULATE ITS NEIGHBOURS
+                    List<Integer> integers =  getNeighbours(array,i,j);
+                    elem.setNeighbours(integers);
+                }
+            }
+
             System.out.print(Arrays.deepToString(array));
+
             repeatCounter--;
         }
         System.out.println();
@@ -64,23 +70,16 @@ public class Main {
     }
 
     private static List<GridElement> makeGridElements(int[][] array) {
-
         List<GridElement> gridElementList = new ArrayList<>();
-
         for (int i = 0; i < array.length; i++) {
-
             for (int j = 0; j < array[i].length; j++) {
-
                 //Creates new ElementObject for each element in the grid
                 GridElement gridElement = new GridElement(i, j,
                         getElementAtPositionColour(array, i, j),
                         getNeighbours(array, i, j));
 
                 gridElementList.add(gridElement);
-
-
             }
-
         }
         return gridElementList;
     }
@@ -107,6 +106,8 @@ public class Main {
     private static void rules(int[][] array, GridElement gridElement) {
 
         List<Integer> neighbours = gridElement.getNeighbours();
+        //TODO SHOULD UPDATE NEW NEIGHBOURS AFTHER RULES APPLICATION
+        //CAN GET NEIGHBOURS FROM THE MATRIX
 
         //Counter for for the number of green neighbours of the index
         int greenCounter = 0;
@@ -119,14 +120,13 @@ public class Main {
             }
         }
 
-
-
         System.out.println();
         if (gridElement.getColour().equals(Colour.RED)) {
             redCellRules(array, gridElement, greenCounter);
         } else if (gridElement.getColour().equals(Colour.GREEN)) {
             greenCellRules(array, gridElement, greenCounter);
         }
+
     }
 
     private static void greenCellRules(int[][] array, GridElement gridElement, int greenCells) {
